7901dc3fca57c19f48c7133f971bbb990a3e74bc
HiddenDongo <kalyankolacahill@gmail.com>
2020-07-09 12:03:42 +0100

    commented

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,31 +1,35 @@
 public class BankAccount {
 
+    // declare private variables for encapsulation
     private String accountNumber;
     private int accountBalance;
 
+    // class constructor
     public BankAccount(String inputAccountNumber, int inputAccountBalance) {
         this.accountNumber = inputAccountNumber;
         this.accountBalance = inputAccountBalance;
         System.out.println("Account " + this.accountNumber + " created successfully with balance " + this.accountBalance);
     }
 
+    // deposit function with appropriate error checking
     public void deposit(int depositAmount) {
         if(depositAmount <= 0) {
             System.out.println("Invalid deposit amount.");
         }
         else {
             this.accountBalance += depositAmount;
-            System.out.println("New balance: " + this.accountBalance);
+            System.out.println("New balance: $" + this.accountBalance);
         }
     }
 
+    // withdraw function with appropriate error checking
     public void withdraw(int withdrawAmount) {
         if(withdrawAmount > this.accountBalance) {
             System.out.println("Withdraw amount exceeds account balance.");
         }
         else {
             this.accountBalance -= withdrawAmount;
-            System.out.println("New balance: " + this.accountBalance);
+            System.out.println("New balance: $" + this.accountBalance);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        // create new bank account with a balance of $100
         BankAccount myAccount = new BankAccount("4D35J", 100);
 
+        // attempt to withdraw and deposit both valid and invalid amounts
         myAccount.deposit(-50);
         myAccount.deposit(50);
         myAccount.withdraw(500);
